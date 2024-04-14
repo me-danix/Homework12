12e440866252ba6cb68e8530a3dc7300384916eb
me-danix <tsilik9801@yandex.ru>
2024-04-14 22:38:17 +0500

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String name;
+    private final Author author;
+    private int publishYear;
+
+    public Book(String name, Author author, int publishYear) {
+        this.name = name;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+}
